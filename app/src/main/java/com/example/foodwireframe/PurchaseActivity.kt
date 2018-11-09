6d85d60bbae8e0351087c2f9299ccead087cf883
BradleyJohnston <bradjohnstonnew@gmail.com>
2018-11-09 01:46:39 -0700

    Updating some things

---
+++ app/src/main/java/com/example/foodwireframe/PurchaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.foodwireframe
+
+import android.app.Activity
+import android.os.Bundle
+
+class PurchaseActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO run initial code for oncreate in this instance
+    }
+}
